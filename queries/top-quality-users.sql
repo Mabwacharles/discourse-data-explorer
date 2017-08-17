fedee1fcd6190dfbb4790f05808818cdd7664b00
SidV <SidVal@users.noreply.github.com>
2017-08-17 16:57:53 -0300

    Create top-quality-users.sql

---
+++ queries/top-quality-users.sql
@@ -0,0 +1,14 @@
+SELECT 
+    sum(p.score) / count(p) as "average score per post", 
+    count(p.id) as post_count, 
+    p.user_id
+FROM posts p
+JOIN users u ON u.id = p.user_id
+WHERE p.created_at >= CURRENT_DATE - INTERVAL '6 month'
+  AND NOT u.admin
+  AND NOT u.blocked
+  AND u.active
+GROUP by user_id, u.views
+HAVING count(p.id) > 50
+ORDER BY sum(p.score) / count(p) DESC
+LIMIT 20
