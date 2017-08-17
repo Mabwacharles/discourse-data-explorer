a0fbb2e11711f31ddeb28b801ab60961f350210c
SidV <SidVal@users.noreply.github.com>
2017-08-17 19:36:06 -0300

    Update users-last-seen.sql

--- queries/users-last-seen.sql
+++ queries/users-last-seen.sql
@@ -1,4 +1,5 @@
 -- https://meta.discourse.org/t/43516?u=sidv
+-- Users Last Seen Since (Since N Weeks Ago)
 
 with intervals as (
     select
@@ -7,6 +8,26 @@ with intervals as (
     from generate_series(CURRENT_TIMESTAMP - INTERVAL '140 days',
                          CURRENT_TIMESTAMP - INTERVAL '7 days',
                          INTERVAL '7 days') n
+)
+select 
+  COUNT(1) as users_seen_since,
+  CURRENT_TIMESTAMP - i.start_time as time_ago
+FROM USERS u
+right join intervals i
+on u.last_seen_at >= i.start_time and u.last_seen_at < i.end_time
+group by i.start_time, i.end_time
+order by i.start_time desc
+
+-- https://meta.discourse.org/t/43516?u=sidv
+-- Users Last Seen Since (Since N Days Ago)
+
+with intervals as (
+    select
+        n as start_time, 
+        CURRENT_TIMESTAMP as end_time
+    from generate_series(CURRENT_TIMESTAMP - INTERVAL '30 days',
+                         CURRENT_TIMESTAMP - INTERVAL '1 day',
+                         INTERVAL '1 day') n
 )
 select 
   COUNT(1) as users_seen_since,
