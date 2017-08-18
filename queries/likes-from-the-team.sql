50749041f5b0afb8e7f60b1b27234573d11568eb
SidV <SidVal@users.noreply.github.com>
2017-08-18 11:28:08 -0300

    Update likes-from-the-team.sql

--- queries/likes-from-the-team.sql
+++ queries/likes-from-the-team.sql
@@ -48,3 +48,56 @@ WHERE pl.user_id NOT IN (
 )
 GROUP BY pl.user_id
 ORDER BY likes DESC
+
+-- End v1 -- 
+
+-- v2 -- untested
+-- [params]
+-- string  :team = 'team'
+
+SELECT
+    pl.user_id,
+    SUM(pl.team_likes) as team_likes,
+    SUM(pl.student_likes) as student_likes,
+    SUM(pl.team_likes + pl.student_likes) as likes
+FROM (
+    SELECT -- count likes per post
+        p.id as post_id_workaround,
+        p.user_id as user_id,
+        (
+            SELECT count(*)
+            FROM post_actions pa
+            WHERE
+                pa.post_id = p.id
+                AND post_action_type_id = (
+                            SELECT id FROM post_action_types WHERE name_key = 'like'
+                )
+                AND pa.user_id IN (
+                    SELECT gu.user_id
+                    FROM group_users gu
+                    WHERE gu.group_id = ( SELECT id FROM groups WHERE name ilike :team ) 
+                )
+        ) as team_likes,
+        (
+            SELECT count(*)
+            FROM post_actions pa
+            WHERE
+                pa.post_id = p.id
+                AND post_action_type_id = (
+                            SELECT id FROM post_action_types WHERE name_key = 'like'
+                )
+                AND pa.user_id NOT IN (
+                    SELECT gu.user_id
+                    FROM group_users gu
+                    WHERE gu.group_id = ( SELECT id FROM groups WHERE name ilike :team ) 
+                )
+        ) as student_likes
+    FROM badge_posts p
+) AS pl
+WHERE pl.user_id NOT IN (
+    SELECT gu.user_id
+    FROM group_users gu
+    WHERE gu.group_id = ( SELECT id FROM groups WHERE name ilike :team ) 
+)
+GROUP BY pl.user_id
+ORDER BY likes DESC
