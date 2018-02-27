450c935fc9e0a966e89a9be83febd56624320489
SidV <SidVal@users.noreply.github.com>
2018-02-27 09:40:36 -0300

    Update top-quality-users.sql

--- queries/top-quality-users.sql
+++ queries/top-quality-users.sql
@@ -15,3 +15,18 @@ GROUP by user_id, u.views
 HAVING count(p.id) > 50
 ORDER BY sum(p.score) / count(p) DESC
 LIMIT 20
+
+-- Modified by SidV: "u.blocked" does not exist anymore.
+SELECT 
+    sum(p.score) / count(p) as "average score per post", 
+    count(p.id) as post_count, 
+    p.user_id
+FROM posts p
+JOIN users u ON u.id = p.user_id
+WHERE p.created_at >= CURRENT_DATE - INTERVAL '6 month'
+  AND NOT u.admin
+  AND u.active
+GROUP by user_id, u.views
+HAVING count(p.id) > 50
+ORDER BY sum(p.score) / count(p) DESC
+LIMIT 20
