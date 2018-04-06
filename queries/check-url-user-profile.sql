a59976b8e682ee5236a088797692b1a0e497ab5e
SidV <SidVal@users.noreply.github.com>
2018-04-06 11:50:31 -0300

    Create check-url-user-profile.sql

---
+++ queries/check-url-user-profile.sql
@@ -0,0 +1,14 @@
+-- https://meta.discourse.org/t/84718/4?u=sidv
+
+-- [params]
+-- int :limit = 150
+-- string :url = %term%
+
+SELECT 
+    up.user_id, u.name, up.website, u.updated_at as updated, up.location, up.views as views
+FROM user_profiles up, users u
+WHERE up.user_id = u.id
+AND (u.admin = 'f' AND u.moderator = 'f')
+AND up.website ILIKE :url
+ORDER BY views desc
+LIMIT :limit
