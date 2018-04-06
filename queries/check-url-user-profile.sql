ad11d4a6b05f177f85b5549bf99ff0a79da9265b
SidV <SidVal@users.noreply.github.com>
2018-04-06 12:10:24 -0300

    Update check-url-user-profile.sql

--- queries/check-url-user-profile.sql
+++ queries/check-url-user-profile.sql
@@ -12,3 +12,19 @@ AND (u.admin = 'f' AND u.moderator = 'f')
 AND up.website ILIKE :url
 ORDER BY views desc
 LIMIT :limit
+
+
+
+-- v2: https://meta.discourse.org/t/5
+
+-- [params]
+-- int :limit = 150
+
+SELECT 
+    up.user_id, u.name, up.website, u.updated_at as updated, up.location, up.views as views
+FROM user_profiles up, users u
+WHERE up.user_id = u.id
+AND (u.admin = 'f' AND u.moderator = 'f')
+AND up.website IS NOT NULL
+ORDER BY views desc
+LIMIT :limit
