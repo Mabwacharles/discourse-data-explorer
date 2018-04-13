fc3f4bc35e448030df518197ec4109f98ad23806
SidV <SidVal@users.noreply.github.com>
2018-04-13 09:28:20 -0300

    Update list-topics-week.sql

--- queries/list-topics-week.sql
+++ queries/list-topics-week.sql
@@ -11,3 +11,18 @@ AND archetype != 'private_message'
 GROUP BY id
 ORDER BY created_at DESC
 LIMIT :limit
+
+-- v2
+-- https://meta.discourse.org/t/85103/7?u=sidv
+
+--[params]
+--string :interval = 1 week
+--int :limit
+SELECT tu.topic_id, t.title, t.created_at as posted
+FROM topics t, topic_users tu
+WHERE t.id = tu.topic_id 
+AND age(t.created_at) < interval :interval
+AND t.archetype != 'private_message'
+GROUP BY tu.topic_id, t.title, t.created_at
+ORDER BY t.created_at DESC
+LIMIT :limit
