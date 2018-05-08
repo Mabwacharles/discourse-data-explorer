451e14417d8b1ac824cee17f2e5c563bc0b80f12
SidV <SidVal@users.noreply.github.com>
2018-05-08 10:37:39 -0300

    Update tracking-referrals.sql

--- queries/tracking-referrals.sql
+++ queries/tracking-referrals.sql
@@ -10,3 +10,19 @@ from invites
 where created_at > CURRENT_TIMESTAMP - INTERVAL :interval
 and user_id > 1
 limit :limit
+
+--v2
+
+-- [params]
+-- int :limit
+-- null string :interval = 91 days
+select i.user_id, 
+    u.username as "Invitado por",
+    i.created_at,
+    i.deleted_at
+from invites i, users u
+where u.id = i.invited_by_id
+and i.created_at > CURRENT_TIMESTAMP - INTERVAL :interval
+and i.user_id > 1
+order by i.created_at desc
+limit :limit
