af9b701ea40caca64ebb49289af572880817485b
SidV <SidVal@users.noreply.github.com>
2018-07-12 13:31:16 -0300

    Create search-term-pm.sql

---
+++ queries/search-term-pm.sql
@@ -0,0 +1,15 @@
+-- https://meta.discourse.org/t/43516/115?u=sidv
+-- Replace WELCOME-TITLE-from-discobot by the exact title of your welcome message, so that all welcome messages generated automatically are excluded.
+
+-- [params]
+-- int :limit = 10
+-- string :term = %term%
+SELECT p.user_id, p.topic_id, p.post_number, p.raw, p.created_at::date
+FROM posts p
+LEFT JOIN topics t on t.id = p.topic_id
+WHERE t.archetype = 'private_message'
+  AND t.title <> 'WELCOME-TITLE-from-discobot'
+  AND p.created_at::date > now()::date - 8
+  AND p.raw ILIKE :term
+ORDER BY p.created_at DESC
+LIMIT :limit
