db51fb594884c6988998f6133ae572a318244e48
SidV <SidVal@users.noreply.github.com>
2018-07-17 16:55:22 -0300

    Update excludes-members-group.sql

--- queries/excludes-members-group.sql
+++ queries/excludes-members-group.sql
@@ -1,5 +1,6 @@
 -- https://meta.discourse.org/t/43516/102?u=sidv
--- To get a list of users that excludes the members of a group, you could try something like this. This will exclude the members of the ‘staff’ group. It should be possible to rework your query to use this.
+-- To get a list of users that excludes the members of a group, you could try something like this. 
+-- This will exclude the members of the ‘staff’ group. It should be possible to rework your query to use this.
 
 WITH group_users AS (
 SELECT user_id
